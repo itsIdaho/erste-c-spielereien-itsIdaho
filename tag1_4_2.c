eaf518fa6b8562abfe3a0c10f119961fabef00db
Jan <43406245+itsIdaho@users.noreply.github.com>
2018-09-25 14:06:46 +0200

    Create tag1_4_2.c

---
+++ tag1_4_2.c
@@ -0,0 +1,8 @@
+{   unsigned int countUpCounter = 0;
+    do {
+    printf("%d\n" , countUpCounter); }
+    while (countDownCounter = countUpCounter++ < 100);
+    printf("Prepare for Landing!\n");
+        
+    return 0;
+}
