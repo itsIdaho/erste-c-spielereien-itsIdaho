6b24f07f0b3b90ee02c964f967e62a1b3ac6ba98
Jan <43406245+itsIdaho@users.noreply.github.com>
2018-09-27 12:54:10 +0200

    Update tag1_4_3.c

--- tag1_4_3.c
+++ tag1_4_3.c
@@ -3,9 +3,11 @@ int main()
 {
     unsigned int zahl = 10;
     do {
-    printf("counter %d\n" ,zahl); }
-    while ( zahl < 10);
-    printf("Prepare for Landing!\n");
+    printf ("counter %d\n" ,zahl); 
+    zahl--;
+    }
+    while ( zahl > 0);
+    printf ("Takeoff!\n");
         
     return 0;
 }
