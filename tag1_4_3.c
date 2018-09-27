0da7563cf827157ce143d1f3dfb7adfc872f076d
Jan <43406245+itsIdaho@users.noreply.github.com>
2018-09-27 12:50:48 +0200

    Create tag1_4_3.c

---
+++ tag1_4_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    unsigned int zahl = 10;
+    do {
+    printf("counter %d\n" ,zahl); }
+    while ( zahl < 10);
+    printf("Prepare for Landing!\n");
+        
+    return 0;
+}
