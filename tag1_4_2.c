0ce955f4177e1ab5e2e46013b681fc1d45646b42
Jan <43406245+itsIdaho@users.noreply.github.com>
2018-09-27 12:35:36 +0200

    Update tag1_4_2.c

--- tag1_4_2.c
+++ tag1_4_2.c
@@ -1,8 +1,12 @@
-{   unsigned int countUpCounter = 0;
-    do {
-    printf("%d\n" , countUpCounter); }
-    while (countDownCounter = countUpCounter++ < 100);
-    printf("Prepare for Landing!\n");
-        
-    return 0;
+#include <stdio.h>
+
+int main()
+{
+	int countDownCounter = 10;
+    while (countDownCounter >= 1)
+	{
+		printf("Counter: %i\n", countDownCounter); countDownCounter--;
+	}
+	printf("Prepare for Ladning!");
+	return 0;
 }
